662234d68a1f4e21c747ddbc29b0def6129b6dbd
Scott Burwell <scott.j.burwell@gmail.com>
2019-07-24 07:40:39 -0500

    misnamed variable in stdize_chanlocs_32scalp_03PNS, fixed

--- example_scripts/alcstress/stdize_chanlocs_32scalp_03PNS.m
+++ example_scripts/alcstress/stdize_chanlocs_32scalp_03PNS.m
@@ -1,4 +1,4 @@
-locations_file = 'curry_sphrad1_n35.ced';
+elec_locations_file = 'curry_sphrad1_n35.ced';
 if exist(elec_locations_file, 'file'), %through this and pre-steps, we lose connection to "urchanlocs"
    chanlocs        = readlocs(elec_locations_file);
    if ~isempty(strmatch('HEO',{EEG.chanlocs.labels},'exact')),
