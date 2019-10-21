a35cbdc27af3c70c961658353268ff8b9b895209
Scott Burwell <scott.j.burwell@gmail.com>
2019-10-21 16:24:06 -0500

    removed NotaBene, startup, and pop_resample (v9) copy

--- startup.m
+++
@@ -1,2 +0,0 @@
-addpath(genpath('/labs/burwellstudy/apps/eeglab13_4_4b')); % pop_resample is from 9_0_2_3b (in eeglab_plugin). necessary for correct trigger assignment. - JH 11.13.16
-addpath(genpath('./'))
